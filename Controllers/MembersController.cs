74dc8818080c955efc26530dad7af3e97a1e585c
JohannKovalenko <johannkovalenko@hotmail.com>
2021-09-13 18:50:56 +0200

    clean controller and action

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -1,37 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using mvc.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace mvc.Controllers
 {
     public class MembersController : Controller
     {
-        private readonly ILogger<MembersController> _logger;
-
-        public MembersController(ILogger<MembersController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Detailed()
+        public ActionResult Detailed()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
