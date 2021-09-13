1d846b0c99496523a019542757de5b24f7a39a04
JohannKovalenko <johannkovalenko@hotmail.com>
2021-09-13 18:42:28 +0200

    add controller and action

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly ILogger<MembersController> _logger;
+
+        public MembersController(ILogger<MembersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detailed()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
